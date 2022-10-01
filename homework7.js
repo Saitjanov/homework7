20843ae0ab4a3f26e8d33dfbb0dc75a7c95cc9a6
Saitjanov <saitjanovtimur@gmail.com>
2022-10-01 16:16:32 +0500

    first commit

---
+++ homework7.js
@@ -0,0 +1,30 @@
+let messi = {
+    name: 'Leo',
+    surname: 'Messi',
+    age: 35,
+    goodGamer: true,
+    isMarried: true,
+    wives:['Alkshka', 'Gulchexra Opa', 'SelenaGomez']
+}
+
+
+let keys = []
+let values = []
+
+console.log(
+    // Object.entries(messi)
+    // Object.keys(messi)
+    // Object.values(messi)
+);
+
+
+
+
+// messi.filter(item => {
+//     if(item.messi) {
+//         keys.messi.push(item)
+//     } else {
+
+//     }
+// })
+
